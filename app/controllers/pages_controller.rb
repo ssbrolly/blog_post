cf1c4112da0306525decfbb4fd2687343162daee
Usong Cho <silvermouse4012@gmail.com>
2018-05-10 19:56:51 -0700

    initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,21 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
   end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+
 end
